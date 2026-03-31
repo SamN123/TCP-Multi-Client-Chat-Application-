d608f3939e75335a8b11ca9caae145855beb0cf4
SamN123 <143456414+SamN123@users.noreply.github.com>
2026-03-31 08:50:38 -0400

    Update mainDriver.c
    added essential main driver functions

--- mainDriver.c
+++ mainDriver.c
@@ -1,13 +1,31 @@
-#include
+#include "server.h"
+#include "client.h"
 
-/*
-    Main driver for multi client echo server 
-*/
-
-int main(void)
+/*Main driver function for server client echo app*/
+int main(int argc, char *argv[])
 {
-    
+    /* Choose mode between server or client*/
+    if (argc != 2)
+    {
+        printf("Usage: %s server|client\n", argv[0]);
+        return 1;
+    }
+
+    /* Start the server mode */
+    if (strcmp(argv[1], "server") == 0)
+    {
+        run_server();
+    }
+    /* Start the client mode */
+    else if (strcmp(argv[1], "client") == 0)
+    {
+        run_client();
+    }
+    else
+    {
+        printf("Invalid mode. Use: server or client\n");
+        return 1;
+    }
+
     return 0;
 }
-
-
